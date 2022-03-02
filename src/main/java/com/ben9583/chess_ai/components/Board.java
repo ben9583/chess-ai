5bc32048b2c365d6ac0df60b61cf1e0e148dfccc
Ben Plate <bplate9583@gmail.com>
2022-03-02 12:07:24 -0800

    remove unnecessary exception

--- src/main/java/com/ben9583/chess_ai/components/Board.java
+++ src/main/java/com/ben9583/chess_ai/components/Board.java
@@ -127,9 +127,8 @@ public class Board {
 
     @NotNull
     public Vector2 removePiece(Piece target) {
-        if(target == null) throw new IllegalArgumentException("Tried to remove piece " + target + ", but it wasn't on the board.");
         Vector2 position = this.getPosition(target);
-
+        
         this.pieces.remove(target);
         this.board[position.getY()][position.getX()] = null;
         return position;
